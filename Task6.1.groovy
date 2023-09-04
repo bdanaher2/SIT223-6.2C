80ee2a3e5a02a7204efc05e50c0c9fad2c5297c4
bdanaher2 <140986822+bdanaher2@users.noreply.github.com>
2023-09-04 11:45:04 +1000

    Add files via upload

---
+++ Task6.1.groovy
@@ -0,0 +1,136 @@
+pipeline {
+
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                echo "Building..."
+            }
+
+            post {
+            success {
+                mail to: "bdanaher2@gmail.com",
+
+                subject: "Build Status Email",
+
+                body: "Build was successful!"
+            }
+            } 
+        }    
+
+        stage('Unit and Integration Tests') {
+            steps {
+                echo "Unit and integration tests..."
+            }
+
+            post {
+                success {
+
+                    mail to: "bdanaher2@gmail.com",
+
+                    subject: "Unit and integration tests Status Email",
+
+                    body: "Unit and integration tests was successful!"
+
+                }
+            }          
+        }
+
+        stage('Code Analysis') {
+            steps {
+                echo "Code Analysis"
+            }
+
+            post {
+                success {
+
+                    mail to: "bdanaher2@gmail.com",
+
+                    subject: "Code Analysis Email",
+
+                    body: "Code Analysis was successful!"
+
+                }
+            }
+        }
+
+        stage('Security Scan') {
+
+            steps {
+                echo "Security Scan"
+            }
+
+            post {
+                success {
+
+                    mail to: "bdanaher2@gmail.com",
+
+                    subject: "Security Scan Status Email",
+
+                    body: "Security Scan was successful!"
+
+                }
+            }          
+        }
+
+        stage('Deploy to Staging') {
+
+            steps {
+                echo "Deploy to staging..."
+            }
+
+            post {
+                success {
+
+                    mail to: "bdanaher2@gmail.com",
+
+                    subject: "Deploy to Staging Status Email",
+
+                    body: "Deploy to Staging was successful!"
+
+                }
+            }
+        }
+
+        stage('Integration Tests on Staging') {
+
+            steps {
+                echo "Integration tests on staging..."
+            }
+
+            post {
+
+                success {
+
+                    mail to: "bdanaher2@gmail.com",
+
+                    subject: "Integration Tests Status Email",
+
+                    body: "Integration Tests was successful!"
+
+                }
+            }
+        }
+
+        stage('Deploy to Production') {
+
+            steps {
+                echo "Deploy to production..."
+            }
+
+            post {
+                success {
+                    mail to: "bdanaher2@gmail.com",
+
+                    subject: "Deploy Status Email",
+
+                    body: "Deploy was successful!"
+
+                }
+
+            }
+
+        }  
+    }
+}
